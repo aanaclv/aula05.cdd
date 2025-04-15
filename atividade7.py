1684833a6fcf575bedbc85b9191971e6a9edb021
ana clara <anaclaraalves675@gmail.com>
2025-04-15 16:40:47 -0300

    atividade7

---
+++ atividade7.py
@@ -0,0 +1,11 @@
+a1 = float(input("Digite sua primeira nota: "))
+a2 = float(input("Digite sua segunda nota: "))
+soma = 0
+while a1 <=10 and a1 >= 0:
+    soma += a1
+    break
+while a2 <=10 and a2 >= 0:
+    soma =+ a2
+    break
+media = soma/2
+print(media)
