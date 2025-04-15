f8650e15c8fc4fb7ff6248f6fa59b8b79ca960e4
ana clara <anaclaraalves675@gmail.com>
2025-04-15 14:35:33 -0300

    atividade 2 e 3

---
+++ atividade3.py
@@ -0,0 +1,14 @@
+numero = 0
+soma = 0
+# enquanto o número for menor que 10
+while numero<10:
+    #input 10 vezes de n
+    n = int(input("Insira um valor: "))
+    # valores n somados na var soma
+    soma += n
+    #quebrar o laço de repetição
+    numero += 1
+
+media = soma/10
+print(media)
+
